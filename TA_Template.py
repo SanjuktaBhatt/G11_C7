893cfdcad1747813ff839a4fb66e3c9bd98c6941
SanjuktaBhatt <64887786+SanjuktaBhatt@users.noreply.github.com>
2021-07-10 17:12:59 +0530

    Create TA_Template.py

---
+++ TA_Template.py
@@ -0,0 +1,64 @@
+import pygame
+pygame.init() 
+WHITE = (255,255,255)
+DARKBLUE = (36,90,190)
+LIGHTBLUE = (0,176,240)
+RED = (255,0,0)
+ORANGE = (255,100,0)
+YELLOW = (255,255,0)
+bricksR=[pygame.Rect(10 + i* 100,60,80,30) for i in range(7)]
+bricksO=[pygame.Rect(10 + i* 100,100,80,30) for i in range(7)]
+bricksY=[pygame.Rect(10 + i* 100,140,80,30) for i in range(7)]
+score = 0
+lives = 3
+velocity=[1,1]
+size = (600, 600)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Breakout Game")
+paddle=pygame.Rect(300,500,60,10)
+ball=pygame.Rect(200,250,10,10)
+carryOn = True
+while carryOn:
+    for event in pygame.event.get(): # User did something
+            if event.type == pygame.QUIT: # If user clicked close
+                  carryOn = False # Flag that we are done so we exit this loop             
+    screen.fill(DARKBLUE)
+    pygame.draw.line(screen, WHITE, [0, 38], [600, 38], 2)
+    pygame.draw.rect(screen,LIGHTBLUE,paddle)
+    # insert score text here
+   
+  
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_RIGHT:
+            if paddle.x<540: 
+                paddle.x+=2
+        if event.key == pygame.K_LEFT:
+            if paddle.x>0:
+                paddle.x-=2
+    for i in bricksR:
+        pygame.draw.rect(screen,RED,i)
+         
+    ball.x+=velocity[0]
+    ball.y+=velocity[1]      
+    if ball.x>=590 or ball.x<=0:
+        velocity[0] = -velocity[0]
+    if ball.y<=38 :
+        velocity[1] = -velocity[1]
+    if paddle.collidepoint(ball.x,ball.y):
+         velocity[1]=-velocity[1]
+   
+    pygame.draw.rect(screen,WHITE ,ball)
+    #Insert code for brick collision
+   
+        #remove brick
+           
+        #reverse direction
+           
+        #increment score
+           
+   
+   
+    pygame.time.wait(1)
+    pygame.display.flip()       
+pygame.quit(  )
+
